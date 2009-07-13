e940f6503fd8d02b31e5db762ea1f7a796b22420
Tim Weber <scy-piraten@scytale.name>
2009-07-14 00:09:32 +0200

    Lokales Bild für CC-BY-SA-Button.

--- _footer.sh
+++ _footer.sh
@@ -9,7 +9,7 @@ fi
 cat <<EOF
 </div>
 <div id="foot">
-<a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/de/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/de/88x31.png" width="88" heigth="31" /></a><br />
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/de/"><img alt="Creative Commons License" style="border-width:0" src="cc-by-sa.png" width="88" heigth="31" /></a><br />
 $work der <a xmlns:cc="http://creativecommons.org/ns#" href="http://podpiraten.de/" property="cc:attributionName" rel="cc:attributionURL">Podpiraten</a> steht unter einer <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/de/">Creative Commons Namensnennung-Weitergabe unter gleichen Bedingungen 3.0 Deutschland Lizenz</a>.<br />
 © $(date +%Y) Podpiraten&nbsp;— <span class="epost">post klammeraffe podpiraten pünktele de</span>&nbsp;— <a href="http://scytale.name/contact/">vorläufiges Impressum</a>
 </div>
