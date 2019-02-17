32b805678314fd535d41930c0f6641d695fe5959
Tim Weber <scy@scy.name>
2019-02-17 16:20:18 +0100

    Pin copyright year to 2009

--- _footer.sh
+++ _footer.sh
@@ -11,7 +11,7 @@ cat <<EOF
 <div id="foot">
 <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/de/"><img alt="Creative Commons License" style="border-width:0" src="cc-by-sa.png" width="88" heigth="31" /></a><br />
 $work der <a xmlns:cc="http://creativecommons.org/ns#" href="http://podpiraten.de/" property="cc:attributionName" rel="cc:attributionURL">Podpiraten</a> steht unter einer <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/de/">Creative Commons Namensnennung-Weitergabe unter gleichen Bedingungen 3.0 Deutschland Lizenz</a>.<br />
-© $(date +%Y) Podpiraten&nbsp;— <span class="epost">post klammeraffe podpiraten pünktele de</span>&nbsp;— <a href="http://scytale.name/contact/">vorläufiges Impressum</a>
+© 2009 Podpiraten&nbsp;— <span class="epost">post klammeraffe podpiraten pünktele de</span>&nbsp;— <a href="http://scytale.name/contact/">vorläufiges Impressum</a>
 </div>
 </div></body>
 </html>
